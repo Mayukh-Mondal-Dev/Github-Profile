f98d5c8d14f216700b7dcf6bda4a1957c101d0d6
Mayukh Mondal <21051405@kiit.ac.in>
2024-01-29 16:24:42 +0530

    Added resume

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+
+@app.route('/download_pdf', methods=['GET'])
+def download_pdf():
+    pdf_path = "static/img/Mayukh Resume Jan 2024.pdf"
+    return send_file(pdf_path, as_attachment=True)
 
 
 if __name__ == '__main__':
